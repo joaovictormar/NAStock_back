ac29aebb19721ed8ce0a7de62621483d0d8fb7dc
João Victor <joaomarquesvictor22@hotmail.com>
2025-02-19 12:12:38 -0300

    Adicionadas rotas no historico

--- src/routes/HistoricoRoutes.js
+++ src/routes/HistoricoRoutes.js
@@ -5,8 +5,11 @@ const historicoController = new HistoricoController();
 
 const router = Router();
 
-router.get("/historico/:id", (req,res) => historicoController.PegaUmRegistro(req,res));
 router.get("/historico/patrimonios", (req,res) => historicoController.PegaTodos(req,res));
+router.get("/historico/:id", (req,res) => historicoController.PegaUmRegistro(req,res));
 router.get("/historico/patrimonios/:patrimonio_id", (req,res) => historicoController.PegaUmRegistro(req,res));
+router.post("/historico/patrimonios", (req,res) => historicoController.criaNovo(req,res));
+router.delete("/historico/:id", (req, res) => historicoController.exclui(req,res));
+
 
 module.exports = router;
