9147d5486b38b670f4d2bdd5c9e0ca8710cc680b
João Victor <joaomarquesvictor22@hotmail.com>
2025-02-19 12:47:33 -0300

    Adicionada organização nas rotas do histórico

--- src/routes/HistoricoRoutes.js
+++ src/routes/HistoricoRoutes.js
@@ -5,10 +5,10 @@ const historicoController = new HistoricoController();
 
 const router = Router();
 
-router.get("/historico/patrimonios", (req,res) => historicoController.PegaTodos(req,res));
+router.get("/historico", (req,res) => historicoController.PegaTodos(req,res));
 router.get("/historico/:id", (req,res) => historicoController.PegaUmRegistro(req,res));
-router.get("/historico/patrimonios/:patrimonio_id", (req,res) => historicoController.PegaRegistrosDeHistoricoDosPatrimonios(req,res));
-router.post("/historico/patrimonios", (req,res) => historicoController.criaNovo(req,res));
+router.get("/historico/patrimonio/:patrimonio_id", (req,res) => historicoController.PegaRegistrosDeHistoricoDosPatrimonios(req,res));
+router.post("/historico", (req,res) => historicoController.criaNovo(req,res));
 router.delete("/historico/:id", (req, res) => historicoController.exclui(req,res));
 
 
