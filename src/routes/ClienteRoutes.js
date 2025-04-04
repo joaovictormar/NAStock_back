a2726f0758dbd7cefba69eedb4e89119197438ea
João Victor <joaomarquesvictor22@hotmail.com>
2025-04-04 14:38:07 -0300

    Adicionados dados ficticios no banco para teste

--- src/routes/ClienteRoutes.js
+++ src/routes/ClienteRoutes.js
@@ -8,5 +8,6 @@ const router = Router();
 router.get('/clientes', (req, res) => clienteController.PegaTodos(req, res));
 router.get('/clientes/:id', (req, res) => clienteController.PegaUmRegistro(req, res));
 router.post('/clientes', (req,res) => clienteController.criaNovo(req,res));
+router.delete('/clientes/:id', (req,res) => clienteController.exclui(req,res));
 
 module.exports = router;
